ddf133aabd7f589f51b52e06fec536ffd31f577a
balhafni <alhafni@usc.edu>
2020-04-26 17:16:12 +0400

    inference script

---
+++ models/inference_seq2seq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p condo 
+# use gpus
+#SBATCH --gres=gpu:1
+# memory
+#SBATCH --mem=120000
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+module purge
+
+export DATA_DIR=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-gender-corpus/
+
+python main.py \
+ --data_dir $DATA_DIR \
+ --embedding_dim 32 \
+ --hidd_dim 64 \
+ --learning_rate 5e-4 \
+ --seed 21 \
+ --model_path /home/ba63/gender-bias/models/saved_models/char_level_nmt_old.pt \
+ --do_inference \
+ --inference_mode dev \
+ --preds_dir /home/ba63/gender-bias/models/dev_preds
