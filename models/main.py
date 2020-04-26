9bed6cc0fdd66765a28fed65121885c2b47c634a
balhafni <alhafni@usc.edu>
2020-04-26 16:13:36 +0400

    minor tweaks

--- models/main.py
+++ models/main.py
@@ -9,7 +9,7 @@ import random
 import numpy as np
 import argparse
 from seq2seq import Seq2Seq
-import logging 
+import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,81 +21,81 @@ class Vectorizer:
         level"""
         self.src_vocab = src_vocab
         self.trg_vocab = trg_vocab
-    
+
     @classmethod
     def create_vectorizer(cls, data_examples):
         """Class method which builds the vectorizer
         vocab"""
-        
+
         src_vocab = SeqVocabulary()
         trg_vocab = SeqVocabulary()
-        
+
         for ex in data_examples:
             src = ex.src
             trg = ex.trg
-            
+
             for t in src:
                 src_vocab.add_token(t)
-                
+
             for t in trg:
                 trg_vocab.add_token(t)
-        
+
         return cls(src_vocab, trg_vocab)
-    
+
     def get_src_indices(self, seq):
         """
         Args:
           - seq (str): The src sequence
-        
+
         Returns:
           - indices (list): <s> + List of tokens to index mapping + </s>
         """
-        indices = [self.src_vocab.sos_idx] 
+        indices = [self.src_vocab.sos_idx]
         indices.extend([self.src_vocab.lookup_token(t) for t in seq])
         indices.append(self.src_vocab.eos_idx)
         return indices
-    
+
     def get_trg_indices(self, seq):
         """
         Args:
           - seq (str): The trg sequence
-        
+
         Returns:
           - trg_x_indices (list): <s> + List of tokens to index mapping
           - trg_y_indices (list): List of tokens to index mapping + </s>
         """
         indices = [self.trg_vocab.lookup_token(t) for t in seq]
-        
+
         trg_x_indices = [self.trg_vocab.sos_idx] + indices
         trg_y_indices = indices + [self.trg_vocab.eos_idx]
         return trg_x_indices, trg_y_indices
-        
+
     def vectorize(self, src, trg):
         """
         Args:
           - src (str): The src sequence
           - src (str): The trg sequence
         Returns:
-          - vectorized_src 
-          - vectorized_trg_x 
+          - vectorized_src
+          - vectorized_trg_x
           - vectorized_trg_y
         """
         src = src
         trg = trg
-        
+
         vectorized_src = self.get_src_indices(src)
         vectorized_trg_x, vectorized_trg_y = self.get_trg_indices(trg)
-        
+
         return {'src': torch.tensor(vectorized_src, dtype=torch.long),
                 'trg_x': torch.tensor(vectorized_trg_x, dtype=torch.long),
                 'trg_y': torch.tensor(vectorized_trg_y, dtype=torch.long)
                }
-    
+
     def to_serializable(self):
         return {'src_vocab': self.src_vocab.to_serializable(),
                 'trg_vocab': self.trg_vocab.to_serializable()
                }
-    
+
     @classmethod
     def from_serializable(cls, contents):
         src_vocab = SeqVocabulary.from_serializable(contents['src_vocab'])
@@ -114,66 +114,65 @@ class MT_Dataset(Dataset):
                              'dev': self.dev_examples,
                              'test': self.test_examples}
         self.set_split('train')
-    
+
     def get_vectorizer(self):
         return self.vectorizer
-    
+
     @classmethod
     def load_data_and_create_vectorizer(cls, data_dir):
         raw_dataset = RawDataset(data_dir)
         # Note: we always create the vectorized based on the train examples
         vectorizer = Vectorizer.create_vectorizer(raw_dataset.train_examples)
         return cls(raw_dataset, vectorizer)
-    
+
     @classmethod
     def load_data_and_load_vectorizer(cls, data_dir, vec_path):
         raw_dataset = RawDataset(data_dir)
         vectorizer = cls.load_vectorizer(vec_path)
         return cls(raw_dataset, vectorizer)
-    
+
     @staticmethod
     def load_vectorizer(vec_path):
         with open(vec_path) as f:
             return Vectorizer.from_serializable(json.load(f))
-    
+
     def save_vectorizer(self, vec_path):
         with open(vec_path, 'w') as f:
             return json.dump(self.vectorizer.to_serializable(), f)
-        
+
     def set_split(self, split):
         self.split = split
         self.split_examples = self.lookup_split[self.split]
         return self.split_examples
-    
+
     def __getitem__(self, index):
         example = self.split_examples[index]
         src, trg = example.src, example.trg
         vectorized = self.vectorizer.vectorize(src, trg)
         return vectorized
-    
+
     def __len__(self):
         return len(self.split_examples)
-    
-    
+
 class Collator:
     def __init__(self, src_pad_idx, trg_pad_idx):
         self.src_pad_idx = src_pad_idx
         self.trg_pad_idx = trg_pad_idx
-        
+
     def __call__(self, batch):
         # Sorting the batch by src seqs length in descending order
         sorted_batch = sorted(batch, key=lambda x: x['src'].shape[0], reverse=True)
-        
+
         src_seqs = [x['src'] for x in sorted_batch]
         trg_x_seqs = [x['trg_x'] for x in sorted_batch]
         trg_y_seqs = [x['trg_y'] for x in sorted_batch]
         lengths = [len(seq) for seq in src_seqs]
-        
+
         padded_src_seqs = pad_sequence(src_seqs, batch_first=True, padding_value=self.src_pad_idx)
         padded_trg_x_seqs = pad_sequence(trg_x_seqs, batch_first=True, padding_value=self.trg_pad_idx)
         padded_trg_y_seqs = pad_sequence(trg_y_seqs, batch_first=True, padding_value=self.trg_pad_idx)
         lengths = torch.tensor(lengths, dtype=torch.long)
-        
+
         return {'src': padded_src_seqs,
                 'trg_x': padded_trg_x_seqs,
                 'trg_y': padded_trg_y_seqs,
@@ -196,12 +195,12 @@ def train(model, dataloader, optimizer, criterion, device='cpu', teacher_forcing
         trg_x = batch['trg_x']
         trg_y = batch['trg_y']
         src_lengths = batch['src_lengths']
-        
-        preds = model(src_seqs=src, 
-                      src_seqs_lengths=src_lengths, 
+
+        preds = model(src_seqs=src,
+                      src_seqs_lengths=src_lengths,
                       trg_seqs=trg_x,
                       teacher_forcing_prob=teacher_forcing_prob)
-        
+
         # CrossEntropysLoss accepts matrices always! 
         # the preds must be of size (N, C) where C is the number 
         # of classes and N is the number of samples. 
@@ -211,10 +210,10 @@ def train(model, dataloader, optimizer, criterion, device='cpu', teacher_forcing
 
         loss = criterion(preds, trg_y)
         epoch_loss += loss.item()
-        
+
         loss.backward()
         optimizer.step()
-        
+
     return epoch_loss / len(dataloader)
 
 def evaluate(model, dataloader, criterion, device='cpu', teacher_forcing_prob=0):
@@ -228,8 +227,8 @@ def evaluate(model, dataloader, criterion, device='cpu', teacher_forcing_prob=0)
             trg_y = batch['trg_y']
             src_lengths = batch['src_lengths']
 
-            preds = model(src_seqs=src, 
-                          src_seqs_lengths=src_lengths, 
+            preds = model(src_seqs=src,
+                          src_seqs_lengths=src_lengths,
                           trg_seqs=trg_x,
                           teacher_forcing_prob=teacher_forcing_prob)
             # CrossEntropyLoss accepts matrices always! 
@@ -238,7 +237,7 @@ def evaluate(model, dataloader, criterion, device='cpu', teacher_forcing_prob=0)
             # The ground truth must be a Vector of size C!
             preds = preds.contiguous().view(-1, preds.shape[-1])
             trg_y = trg_y.view(-1)
-            
+
             loss = criterion(preds, trg_y)
             epoch_loss += loss.item()
 
@@ -248,8 +247,8 @@ def evaluate(model, dataloader, criterion, device='cpu', teacher_forcing_prob=0)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--data_dir", 
-        default=None, 
+        "--data_dir",
+        default=None,
         type=str,
         required=True,
         help="The input data dir. Should contain the src and trg files."
@@ -291,7 +290,7 @@ def main():
     parser.add_argument(
         "--learning_rate",
         default=5e-4,
-        type=int,
+        type=float,
         help="The initial learning rate for Adam."
     )
     parser.add_argument(
@@ -385,33 +384,33 @@ def main():
         best_loss = 1e10
         teacher_forcing_prob = 0.3
         set_seed(args.seed, args.use_cuda)
-        for epoch in range(args.num_epochs):
+        for epoch in range(args.num_train_epochs):
             dataset.set_split('train')
             dataloader = DataLoader(dataset, shuffle=True, batch_size=args.batch_size, collate_fn=collator)
 
             train_loss = train(model, dataloader, optimizer, criterion, device, teacher_forcing_prob=teacher_forcing_prob)
             train_losses.append(train_loss)
-            
+
             dataset.set_split('dev')
             dataloader = DataLoader(dataset, shuffle=True, batch_size=args.batch_size, collate_fn=collator)
             dev_loss = evaluate(model, dataloader, criterion, device, teacher_forcing_prob=0)
             dev_losses.append(dev_loss)
-            
+
             #save best model
             if dev_loss < best_loss:
                 best_loss = dev_loss
                 torch.save(model.state_dict(), args.model_path)
-            
+
             scheduler.step(dev_loss)
             logger.info(f'Epoch: {(epoch + 1)}')
             logger.info(f'\tTrain Loss: {train_loss:.4f}   |   Dev Loss: {dev_loss:.4f}')
-        
+
         if args.do_eval:
             logger.info('Evaluation')
             collator = Collator(SRC_PAD_INDEX, TRG_PAD_INDEX)
             set_seed(args.seed, args.use_cuda)
             dev_losses = []
-            for epoch in range(args.num_epochs):
+            for epoch in range(args.num_train_epochs):
                 dataset.set_split('dev')
                 dataloader = DataLoader(dataset, shuffle=True, batch_size=args.batch_size, collate_fn=collator)
                 dev_loss = evaluate(model, dataloader, criterion, device, teacher_forcing_prob=0)
