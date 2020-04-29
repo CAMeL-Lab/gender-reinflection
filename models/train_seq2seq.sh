359e3f6720104d9d778d6fff208ee6c2451d69f8
balhafni <alhafni@usc.edu>
2020-04-29 16:17:13 +0400

    updating training script

--- models/train_seq2seq.sh
+++ models/train_seq2seq.sh
@@ -17,13 +17,17 @@ export DATA_DIR=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-ge
 python main.py \
  --data_dir $DATA_DIR \
  --vectorizer_path /home/ba63/gender-bias/models/saved_models/vectorizer.json \
+ --analyzer_db_path /scratch/ba63/databases/calima-msa/calima-msa.0.2.2.utf8.db \
+ --morph_features_path /home/ba63/gender-bias/models/saved_models/morph_features.json \
+ --use_morph_features \
  --cache_files \
  --num_train_epochs 50 \
- --embedding_dim 32 \
- --hidd_dim 64 \
+ --embedding_dim 128 \
+ --hidd_dim 256 \
  --learning_rate 5e-4 \
  --batch_size 32 \
  --use_cuda \
  --seed 21 \
  --do_train \
- --model_path /home/ba63/gender-bias/models/saved_models/char_level_nmt_old.pt
+ --visualize_loss \
+ --model_path /home/ba63/gender-bias/models/saved_models/char_level_big_improv_256_128_gender.pt
