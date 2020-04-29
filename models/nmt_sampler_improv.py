27a4f6db7dd35607fe0c59583f33da1aad66ce86
balhafni <alhafni@usc.edu>
2020-04-29 16:05:15 +0400

    tweaking sampler

--- models/nmt_sampler_improv.py
+++ models/nmt_sampler_improv.py
@@ -1,22 +1,31 @@
 import torch
 import numpy as np
+import re
 
 class NMT_Batch_Sampler:
-    def __init__(self, model, src_vocab, trg_vocab):
+    def __init__(self, model, src_vocab_char, src_vocab_word, trg_vocab):
         self.model = model
-        self.src_vocab = src_vocab
+        self.src_vocab_char = src_vocab_char
+        self.src_vocab_word = src_vocab_word
         self.trg_vocab = trg_vocab
 
     def update_batch(self, batch):
         self.sample_batch = batch
 
-        src = batch['src']
+        src_char = batch['src_char']
+        src_word = batch['src_word']
         trg_x = batch['trg_x']
         trg_y = batch['trg_y']
         src_lengths = batch['src_lengths']
-        preds, attention_scores = self.model(src, src_lengths, trg_x, teacher_forcing_prob=0)
+
+        preds, attention_scores = self.model(src_char,
+                                            src_word,
+                                            src_lengths,
+                                            trg_x,
+                                            teacher_forcing_prob=0)
+
         # preds shape: [batch_size, trg_seq_len, output_dim]
-        
+
         self.sample_batch['preds'] = preds
         self.sample_batch['attention_scores'] = attention_scores
 
@@ -33,8 +42,8 @@ class NMT_Batch_Sampler:
         return self.get_str_sentence(trg_sentence, self.trg_vocab)
 
     def get_src_sentence(self, index):
-        src_sentence = self.sample_batch['src'][index].cpu().detach().numpy()
-        return self.get_str_sentence(src_sentence, self.src_vocab)
+        src_sentence = self.sample_batch['src_char'][index].cpu().detach().numpy()
+        return self.get_str_sentence(src_sentence, self.src_vocab_char)
 
     def get_str_sentence(self, vectorized_sentence, vocab):
         sentence = []
@@ -48,24 +57,36 @@ class NMT_Batch_Sampler:
         return ''.join(sentence)
 
     def translate_sentence(self, sentence, max_len=120):
-        # vectorizing the src sentence
-        vectorized_src_sentence = [self.src_vocab.sos_idx]
-        vectorized_src_sentence.extend([self.src_vocab.lookup_token(t) for t in sentence])
-        vectorized_src_sentence.append(self.src_vocab.eos_idx)
+        # vectorizing the src sentence on the char level and word level
+
+        sentence = re.split(r'(\s+)', sentence)
+        vectorized_src_sentence_char = [self.src_vocab_char.sos_idx]
+        vectorized_src_sentence_word = [self.src_vocab_word.sos_idx]
+
+        for word in sentence:
+            for c in word:
+                vectorized_src_sentence_char.append(self.src_vocab_char.lookup_token(c))
+                vectorized_src_sentence_word.append(self.src_vocab_word.lookup_token(word))
+
+        vectorized_src_sentence_word.append(self.src_vocab_word.eos_idx)
+        vectorized_src_sentence_char.append(self.src_vocab_char.eos_idx)
 
         # getting sentence length
-        src_sentence_length = [len(vectorized_src_sentence)]
+        src_sentence_length = [len(vectorized_src_sentence_char)]
 
         # converting the lists to tensors
-        vectorized_src_sentence = torch.tensor([vectorized_src_sentence], dtype=torch.long)
+        vectorized_src_sentence_char = torch.tensor([vectorized_src_sentence_char], dtype=torch.long)
+        vectorized_src_sentence_word = torch.tensor([vectorized_src_sentence_word], dtype=torch.long)
         src_sentence_length = torch.tensor(src_sentence_length, dtype=torch.long)
 
         # passing the src sentence to the encoder
         with torch.no_grad():
-            encoder_outputs, encoder_h_t= self.model.encoder(vectorized_src_sentence, src_sentence_length)
+            encoder_outputs, encoder_h_t= self.model.encoder(vectorized_src_sentence_char,
+                                                             vectorized_src_sentence_word,
+                                                             src_sentence_length)
 
         # creating attention mask
-        attention_mask = self.model.create_mask(vectorized_src_sentence, self.src_vocab.pad_idx)
+        attention_mask = self.model.create_mask(vectorized_src_sentence_char, self.src_vocab_char.pad_idx)
 
         # initilizating the first decoder_h_t to encoder_h_t
         decoder_h_t = encoder_h_t
@@ -81,10 +102,10 @@ class NMT_Batch_Sampler:
                 y_t = torch.tensor([trg_seqs[-1]], dtype=torch.long)
 
                 # do a single decoder step
-                prediction, decoder_h_t, atten_scores, context_vectors = self.model.decoder(y_t, 
-                                                                                          encoder_outputs, 
-                                                                                          decoder_h_t, 
-                                                                                          context_vectors, 
+                prediction, decoder_h_t, atten_scores, context_vectors = self.model.decoder(y_t,
+                                                                                          encoder_outputs,
+                                                                                          decoder_h_t,
+                                                                                          context_vectors,
                                                                                           attention_mask=attention_mask)
 
                 # getting the most probable prediciton
