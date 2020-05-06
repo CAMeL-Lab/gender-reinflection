93f89540fd15b0788d5471abc6f8ca10d147f720
balhafni <alhafni@usc.edu>
2020-05-06 22:21:38 +0400

    fasttext training script

---
+++ models/fasttext_opensub_train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p serial 
+# Set number of nodes to run
+#SBATCH --nodes=1
+# Set number of tasks to run
+#SBATCH --ntasks=1
+# Set number of cores per task (default is 1)
+#SBATCH --cpus-per-task=15
+# memory
+#SBATCH --mem=50GB
+# Walltime format hh:mm:ss
+#SBATCH --time=48:00:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+module load gcc
+
+export INPUT=/scratch/ba63/gender_bias/data/OpenSubtitles.ar-en.ar.sep.punc
+export OUTPUT=/scratch/ba63/gender_bias/data/OpenSubtitles_fasttext_embeddings
+
+/home/ba63/fastText-0.9.1/fasttext skipgram -input $INPUT -minn 2 -maxn 5 -dim 300 -epoch 5 -output $OUTPUT
