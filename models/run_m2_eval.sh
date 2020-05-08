71062d23709315c74734088f18abe3891c05717c
balhafni <alhafni@usc.edu>
2020-05-08 17:05:19 +0400

    m2 evaluation script tweaks

--- models/run_m2_eval.sh
+++ models/run_m2_eval.sh
@@ -5,9 +5,9 @@
 # Set number of tasks to run
 #SBATCH --ntasks=1
 # Set number of cores per task (default is 1)
-#SBATCH --cpus-per-task=24 
+#SBATCH --cpus-per-task=2 
 # memory
-#SBATCH --mem=120000
+#SBATCH --mem=10GB
 # Walltime format hh:mm:ss
 #SBATCH --time=11:30:00
 # Output and error files
@@ -18,7 +18,8 @@ module purge
 eval "$(conda shell.bash hook)"
 conda activate python2
 
-export SYSTEM_HYP=/home/ba63/gender-bias/models/dev_preds
-export GOLD_DATA=/home/ba63/gender-bias/data/christine_2019/Arabic-parallel-gender-corpus/edits_annotations/D-set-dev.arin.to.M.edits_annotation
+export EXPERIMENT_NAME=M.to.F
+export SYSTEM_HYP=/home/ba63/gender-bias/models/new_decoder_inferences/$EXPERIMENT_NAME/dev_preds_improv_256_128_gender_1e-6_wout_trg.inf
+export GOLD_DATA=/home/ba63/gender-bias/data/christine_2019/Arabic-parallel-gender-corpus/edits_annotations/D-set-dev.$EXPERIMENT_NAME.edits_annotation
 
-python /home/ba63/m2scorer/scripts/m2scorer.py $SYSTEM_HYP $GOLD_DATA 
+python /home/ba63/m2scorer/scripts/m2scorer.py $SYSTEM_HYP $GOLD_DATA > new_decoder_inferences/$EXPERIMENT_NAME/dev_preds_improv_256_128_gender_1e-6_wout_trg.inf.eval
