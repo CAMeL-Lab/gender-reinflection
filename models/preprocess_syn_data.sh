a7849ce5a4d9efde04e4936dd93bc6941d918a50
balhafni <alhafni@usc.edu>
2020-06-04 23:53:12 +0400

    adding scratch output dirs

--- models/preprocess_syn_data.sh
+++ models/preprocess_syn_data.sh
@@ -18,7 +18,7 @@ module purge
 
 export SRC_INPUT_FILE=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-gender-corpus/S-set.M
 export TRG_INPUT_FILE=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-gender-corpus/S-set.F
-export SRC_OUTPUT_FILE=/home/ba63/test.M
-export TRG_OUTPUT_FILE=/home/ba63/test.F
+export SRC_OUTPUT_FILE=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-gender-corpus/S-set.M.uniq
+export TRG_OUTPUT_FILE=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-gender-corpus/S-set.F.uniq
 
 python preprocess_syn_data.py --src_input_file $SRC_INPUT_FILE --trg_input_file $TRG_INPUT_FILE --src_output_file $SRC_OUTPUT_FILE --trg_output_file $TRG_OUTPUT_FILE
