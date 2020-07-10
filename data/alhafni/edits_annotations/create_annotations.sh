024ae3fc3b57995c2ee282f1fb08406c76401981
balhafni <alhafni@usc.edu>
2020-07-10 18:21:16 +0400

    M2 scorer edits creation script

--- data/alhafni/edits_annotations/create_annotations.sh
+++ data/alhafni/edits_annotations/create_annotations.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+#SBATCH -p condo 
+# Set number of nodes to run
+#SBATCH --nodes=1
+# Set number of tasks to run
+#SBATCH --ntasks=1
+# Set number of cores per task (default is 1)
+#SBATCH --cpus-per-task=10 
+# memory
+#SBATCH --mem=10GB
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
 # TODO: find a better way to provide inputs and ouputs to this script
-python  ~/m2scorer/scripts/edit_creator.py D-set-dev.ar.F D-set-dev.ar.M > edits_annotations/D-set-dev.F.to.M.edits_annotation
+
+module purge
+eval "$(conda shell.bash hook)"
+conda activate python2
+
+python  ~/m2scorer/scripts/edit_creator.py D-set-dev.ar.F D-set-dev.ar.M > edits_annotations/D-set-dev.F.to.M.edits_annotationa
 python  ~/m2scorer/scripts/edit_creator.py D-set-dev.ar.M D-set-dev.ar.F > edits_annotations/D-set dev.M.to.F.edits_annotation
 python  ~/m2scorer/scripts/edit_creator.py D-set-dev.arin D-set-dev.ar.M > edits_annotations/D-set-dev.arin.to.M.edits_annotation
 python  ~/m2scorer/scripts/edit_creator.py D-set-dev.arin D-set-dev.ar.F > edits_annotations/D-set-dev.arin.to.F.edits_annotation
