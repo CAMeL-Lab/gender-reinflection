c0fd1861dd20253024e15506a95a2429cb9c0685
balhafni <alhafni@usc.edu>
2020-07-13 20:20:56 +0400

    seq2seq models inference script

--- scripts/inference_seq2seq.sh
+++ scripts/inference_seq2seq.sh
@@ -13,25 +13,20 @@
 nvidia-smi
 module purge
 
-export DATA_DIR=/scratch/ba63/gender_bias/data/christine_2019/Arabic-parallel-gender-corpus/
-export EXPERIMENT_NAME=arin.to.M
-
-#--use_morph_features \
-#--use_fasttext_embeddings \
-#--fasttext_embeddings_kv_path /scratch/ba63/gender_bias/data/fasttext_embeddings/OpenSubtitles_fasttext_embeddings_100.kv \
+export DATA_DIR=data/alhafni
 
 python main.py \
  --data_dir $DATA_DIR \
+ --embed_trg_gender \
  --analyzer_db_path /scratch/ba63/databases/calima-msa/calima-msa.0.2.2.utf8.db \
  --use_morph_features \
- --embed_trg_gender \
  --trg_gender_embedding_dim 10 \
  --embedding_dim 128 \
  --hidd_dim 256 \
  --num_layers 2 \
  --learning_rate 5e-4 \
  --seed 21 \
- --model_path /home/ba63/gender-bias/models/saved_models/joint_models/char_level_256_128_2_layers_w_morph_top_1_analyses_w_trg_clip_norm_new_enc_new_no_bias_v_char_10_trg_no_last.pt \
+ --model_path saved_models/joint_models/joint+morph.pt \
  --do_inference \
  --inference_mode dev \
- --preds_dir /home/ba63/gender-bias/models/logs/joint_models/dev_preds_256_128_2_layers_w_morph_top_1_analyses_w_trg_clip_norm_new_enc_new_no_bias_v_char_10_trg_no_last_test
+ --preds_dir logs/reinflection/joint_models/dev.joint+morph_blabla
