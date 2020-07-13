58909099c34574b2cd93200863a4741c4b045d4a
balhafni <alhafni@usc.edu>
2020-07-13 20:40:34 +0400

    joint models reinfelction eval script

---
+++ scripts/run_eval_norm_joint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH -p condo 
+# Set number of nodes to run
+#SBATCH --nodes=1
+# Set number of tasks to run
+#SBATCH --ntasks=1
+# Set number of cores per task (default is 1)
+#SBATCH --cpus-per-task=10 
+# memory
+#SBATCH --mem=10GB
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+module purge
+
+export DATA_SPLIT=test
+export EXPERIMENT_NAME=arin+arin.to.D-set-$DATA_SPLIT.ar.M+F
+export GOLD_DATA=D-set-$DATA_SPLIT.ar.M+D-set-$DATA_SPLIT.ar.F.normalized
+export EDITS_ANNOTATIONS=D-set-$DATA_SPLIT.$EXPERIMENT_NAME.edits_annotation.normalized
+
+export SYSTEM_HYP=logs/reinflection/joint_models/$DATA_SPLIT.joint+morph.inf.norm
+
+export GOLD_ANNOTATION=data/alhafni/edits_annotations_normalized/$EDITS_ANNOTATIONS
+
+export TRG_GOLD_DATA=data/alhafni/joint_model/$GOLD_DATA
+
+# run M2 Scorer evaluation
+eval "$(conda shell.bash hook)"
+conda activate python2
+
+m2_eval=$(python /home/ba63/m2scorer/scripts/m2scorer.py $SYSTEM_HYP $GOLD_ANNOTATION)
+
+conda activate python3
+
+# run accuracy evaluation
+accuracy=$(python utils/metrics.py --trg_directory $TRG_GOLD_DATA --pred_directory $SYSTEM_HYP)
+
+# run BLEU evaluation
+bleu=$(cat $SYSTEM_HYP | sacrebleu $TRG_GOLD_DATA --force --short)
+
+printf "%s\n%s\n%-12s%s" "$m2_eval" "$accuracy" "BLEU" ": $bleu" > eval
